fb6cfc927e074b4f1e87fb2115256fe6d4a33b40
h-zahar <hz.rifat@gmail.com>
2023-06-14 07:52:13 +0600

    Customized Notification Period

--- background.js
+++ background.js
@@ -10,11 +10,20 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 
     chrome.action.setBadgeText({ text: `${time + 1}` });
 
-    if (time % 60 === 0) {
-      this.registration.showNotification("Timer", {
-        body: `${Math.floor(time / 60)} minutes passed!`,
-        icon: "icon.png",
-      });
-    }
+    chrome.storage.sync.get(["timeNotification"], (data) => {
+      if (
+        data?.timeNotification &&
+        time % (data?.timeNotification ? data.timeNotification * 60 : -1) === 0
+      ) {
+        this.registration.showNotification("Timer", {
+          body: `${Math.floor(time / 60)} minutes passed!`,
+          icon: "icon.png",
+        });
+      }
+    });
   });
 });
+
+chrome.storage.sync.get(["name", "timeNotification"], (data) =>
+  console.log(data)
+);
--- options.js
+++ options.js
@@ -1,15 +1,19 @@
 const nameInput = document.getElementById("input-name");
+const timeNotificationElem = document.getElementById("input-notification");
 
 const saveBtn = document.getElementById("btn-save");
 
 saveBtn.addEventListener("click", () => {
   const name = nameInput.value;
-  chrome.storage.sync.set({ name }, () => {
-    console.log(`Name is set to: ${name}`);
-  });
+  const timeNotification = timeNotificationElem.value;
+
+  chrome.storage.sync.set({ name, timeNotification });
 });
 
 chrome.storage.sync.get(["name"], (data) => {
   //   console.log(data);
   nameInput.value = data.name ? data.name : "";
+  timeNotificationElem.value = data.timeNotification
+    ? data.timeNotification
+    : 10;
 });
