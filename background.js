d506403ff1d3837b67a1bf4117d01550372213ad
h-zahar <hz.rifat@gmail.com>
2023-06-15 05:11:31 +0600

    Merged a Scope

--- background.js
+++ background.js
@@ -5,28 +5,25 @@ chrome.alarms.create({
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  chrome.storage.local.get(["state"], (data) => {
+  chrome.storage.local.get(["timer", "state"], (data) => {
     if (data.state === "STOP" || data.state === "PAUSE") return;
 
-    chrome.storage.local.get(["timer"], (data) => {
-      const time = data.timer ? data.timer : 0;
+    const time = data.timer ? data.timer : 0;
 
-      chrome.storage.local.set({ timer: time + 1 });
+    chrome.storage.local.set({ timer: time + 1 });
 
-      chrome.action.setBadgeText({ text: `${time + 1}` });
+    chrome.action.setBadgeText({ text: `${time + 1}` });
 
-      chrome.storage.sync.get(["timeNotification"], (data) => {
-        if (
-          data?.timeNotification &&
-          time % (data?.timeNotification ? data.timeNotification * 60 : -1) ===
-            0
-        ) {
-          this.registration.showNotification("Timer", {
-            body: `${Math.floor(time / 60)} minutes passed!`,
-            icon: "icon.png",
-          });
-        }
-      });
+    chrome.storage.sync.get(["timeNotification"], (data) => {
+      if (
+        data?.timeNotification &&
+        time % (data?.timeNotification ? data.timeNotification * 60 : -1) === 0
+      ) {
+        this.registration.showNotification("Timer", {
+          body: `${Math.floor(time / 60)} minutes passed!`,
+          icon: "icon.png",
+        });
+      }
     });
   });
 });
