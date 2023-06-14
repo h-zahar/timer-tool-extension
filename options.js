8db4c26509c014fb9c6574b47ff4871c2f41941a
h-zahar <hz.rifat@gmail.com>
2023-06-14 06:11:59 +0600

    Integrated Storage API

--- options.js
+++ options.js
@@ -3,5 +3,13 @@ const nameInput = document.getElementById("input-name");
 const saveBtn = document.getElementById("btn-save");
 
 saveBtn.addEventListener("click", () => {
-  console.log(nameInput.value);
+  const name = nameInput.value;
+  chrome.storage.sync.set({ name }, () => {
+    console.log(`Name is set to: ${name}`);
+  });
+});
+
+chrome.storage.sync.get(["name"], (data) => {
+  //   console.log(data);
+  nameInput.value = data.name ? data.name : "";
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 const timeElem = document.getElementById("time");
 
+const nameElem = document.getElementById("name");
+
 const currTime = new Date().toLocaleTimeString();
 //     .split(" ")[0]
 //     .split(":")
@@ -12,4 +14,8 @@ timeElem.textContent = `The time is: ${currTime}`;
 
 chrome.action.setBadgeText({ text: "Time" }, () => {
   console.log("Setting Badge Text");
+});
+
+chrome.storage.sync.get(["name"], (data) => {
+  nameElem.textContent = `Hello ${data.name ? data.name : "Stranger"}`;
 });
